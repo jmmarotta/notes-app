70bea5e208e5bb7894244c50adafa7318110d7ec
Julian Marotta <julianmmarotta@gmail.com>
2021-09-14 22:30:27 -0400

    Added Ruby Notes address for email

--- app/mailers/note_mailer.rb
+++ app/mailers/note_mailer.rb
@@ -3,6 +3,6 @@ class NoteMailer < ApplicationMailer
     name = params[:name]
     @email = params[:to_email]
     @note = params[:note]
-    mail(to: @email, subject: "Note from #{name}")
+    mail(to: @email, subject: "#{name} sent you a Ruby-Note")
   end
 end
